b268399d40be36f79b0293be37f499f0e28cc99f
Bangaly Kake <bangaly.kake@gmail.com>
2017-01-20 20:29:52 +0100

    Add user repository

---
+++ tutoSpring-core/src/main/java/fr/free/bkake/core/repository/UserRespository.java
@@ -0,0 +1,10 @@
+package fr.free.bkake.core.repository;
+
+
+import fr.free.bkake.core.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+
+public interface UserRespository extends JpaRepository<User, Long>, QueryDslPredicateExecutor<User> {
+	User findByLogin(final String login);
+}
