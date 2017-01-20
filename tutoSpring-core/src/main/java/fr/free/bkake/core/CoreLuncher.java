f0d10e7a7c4f2e594a79adb7b32629703c7b202b
Bangaly Kake <bangaly.kake@gmail.com>
2017-01-20 20:21:58 +0100

    Initialize spring boot application in core module

---
+++ tutoSpring-core/src/main/java/fr/free/bkake/core/CoreLuncher.java
@@ -0,0 +1,14 @@
+package fr.free.bkake.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CoreLuncher {
+
+    public static void main(String... args) {
+        SpringApplication.run(CoreLuncher.class);
+    }
+
+}
